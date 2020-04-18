7429af38610af99c74e2a271a439d2e44fba1274
Willian Home <willian@wiki4fit.com.br>
2020-04-18 17:57:45 -0300

    Desafio finalizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Unform ${Date.now()}`,
+      url: 'https://github.com/Rocketseat/unform',
+      techs: ["javascript", "node"]
+    });
+
+    setRepositories([ ...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    if(response.status === 204) {
+      setRepositories(repositories.filter(repositorie => repositorie.id !== id));
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repositorie => (
+            <li key={repositorie.id}>
+              {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repositorie.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
